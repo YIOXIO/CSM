2cfaada00efcdb7be055f0a83afd9a2b84061974
Konstantin-Trofimov <const.trofimov@gmail.com>
2021-12-03 17:21:37 +0300

    анимация для диаграмм в блоке наука и центральном блоке bug fix

--- home-page/main/src/js/main.js
+++ home-page/main/src/js/main.js
@@ -736,7 +736,7 @@ document.addEventListener('DOMContentLoaded', () => {
             } 
             else {
                 
-                diagrAnimCounter = 0
+                publicationsDiagramCounter = 0
             }
             
             publicationsDiagramAnimation()
