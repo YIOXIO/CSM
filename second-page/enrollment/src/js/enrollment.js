0b095cdc98581747c0aa6b12aab4d3f1aff11998
Konstantin Trofimov <const.trofimov@gmail.com>
2022-05-17 14:45:50 +0300

    Create page `enrollment`

---
+++ second-page/enrollment/src/js/enrollment.js
@@ -0,0 +1,19 @@
+const tabs = document.querySelectorAll('.enrollment__tab')
+const tabcontent = document.querySelectorAll('.enrollment__tabcontent')
+
+tabs.forEach((tab, index) => {
+ tab.addEventListener('click', () => {
+  tabcontent.forEach(item => item.classList.remove('show'))
+  tabcontent.forEach(item => item.classList.add('hide'))
+  tabcontent[index].classList.add('show')
+
+  tabs.forEach(item => {
+   item.querySelector('svg').querySelector('path').setAttribute('stroke', 'rgba(33, 122, 255, 0.2)')
+   item.classList.remove('enrollment__tab_active')
+  })
+
+  tab.querySelector('svg').querySelector('path').setAttribute('stroke', '#217AFF')
+  tab.classList.add('enrollment__tab_active')
+  
+ })
+})
