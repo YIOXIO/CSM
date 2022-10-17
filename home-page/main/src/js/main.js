f2ada014d359305fba8999559509635299acf86d
Konstantin Trofimov <const.trofimov@gmail.com>
2022-10-17 17:43:50 +0300

    Add vaccination  block, reomove finance  block, change dpo block position, change central diagram

--- home-page/main/src/js/main.js
+++ home-page/main/src/js/main.js
@@ -196,15 +196,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const generalData = {
         students:  4844,
-        // rate: 5,
+        rate: 5,
         teachers: 1279,
         scientists: 347,
         score: 4.2,
         contracts: 111,
         progress: 39.62,
         publications: 1078,
-        cash: 3.45,
-        budget: 4.08,
+        // cash: 3.45,
+        // budget: 4.08,
         dpoProgress: 30.8,
         dpoStudents: 11009  
     }
@@ -332,8 +332,8 @@ document.addEventListener('DOMContentLoaded', () => {
     
     }
     
-    new Diagram('Фонд оплаты труда', getInt(wageFundDiagramData), ['#2BD6FB', '#217AFF'], 'finance__diagram_wage-fund', ).render()
-    new Diagram('Затраты', getInt(сostsDiagramData), ['#FFC01D',  '#FB9B2B', '#FD6A6A'], 'finance__diagram_сosts', true).render()
+    // new Diagram('Фонд оплаты труда', getInt(wageFundDiagramData), ['#2BD6FB', '#217AFF'], 'finance__diagram_wage-fund', ).render()
+    // new Diagram('Затраты', getInt(сostsDiagramData), ['#FFC01D',  '#FB9B2B', '#FD6A6A'], 'finance__diagram_сosts', true).render()
 
     function financeDataOutput(data) {
         insertToPage('finance__total-value', numDataOutput(data.total));
@@ -345,7 +345,7 @@ document.addEventListener('DOMContentLoaded', () => {
         insertToPage('finance__diagram-value_property-tax', numDataOutput(data.propertyTax));
     }
 
-    financeDataOutput(financeData);
+    // financeDataOutput(financeData);
 
     function setSpace(elem) {
         let strLength = elem.textContent.length;
@@ -361,7 +361,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    document.querySelectorAll('.finance__diagram-value').forEach(i => setSpace(i))
+    // document.querySelectorAll('.finance__diagram-value').forEach(i => setSpace(i))
 
 
 
@@ -667,6 +667,64 @@ document.addEventListener('DOMContentLoaded', () => {
     
     publicationsDiagramAnimation()
 
+     //=================== вакцинация =====================
+
+     const vaccinationData = {
+        date: '2021-10-02',
+        total: 3345,
+        vaccinated: 2457,
+        unvaccinated: 887,
+        recovered: 1050,
+        unsuitable: 232,
+        sick: 108
+    }
+
+    function vaccinationDataOutput(data) {
+
+        function sumData() {
+            const result = [data.vaccinated, data.unvaccinated, data.recovered, data.sick, data.unsuitable];
+            const sum = result.reduce((a, b) => a + b)
+
+            return sum;
+        } 
+
+        const categoryRatio = category => Math.ceil((category / sumData()) * 100);
+
+        const setLine = (value, color) => {
+            return  `
+                <svg width="100%" overflow="visible" viewBox="0 0 120 3" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <svg class="vaccination__graphiс-data" width="${categoryRatio(value + 1)}" x="0" y="0" >
+                    <rect class="vaccination__graphiс-row" fill=${color} x="0" y="0" transform="translate(-5)"/>
+                    <text class="vaccination__graphiс-value" x="100%" y="60%"  text-anchor="middle" dy="0.3em" transform="translate(5)">
+                        ${value}
+                    </text>
+                </svg>
+            </svg>`
+        }
+                
+        function output (data) {
+            const element = document.createElement('div');
+
+            element.classList.add('vaccination__graphiс');
+
+            element.innerHTML = `
+                ${setLine(data.vaccinated,'#217AFF')}
+                ${setLine(data.unvaccinated, '#6C38FF')}
+                ${setLine(data.recovered, '#A7EB17')}
+                ${setLine(data.unsuitable, '#FB9B2B' )}
+                ${setLine(data.sick, '#FD6A6A')}
+           
+            `;
+
+            document.querySelector('.vaccination__diagram-wrap').append(element);
+        }
+
+        output(data);
+        document.querySelector('.vaccination__diagram-value').textContent = data.total;
+    }
+
+    vaccinationDataOutput(vaccinationData);
+
     // ============================== студенты ===========================================
 
 
@@ -933,7 +991,7 @@ document.addEventListener('DOMContentLoaded', () => {
         customElements.get('single-calendar') || customElements.define('single-calendar', CalendarSingle);
     }
       
-    document.querySelector('.calendar-finance').setAttribute('data-date', financeData.date);
+    // document.querySelector('.calendar-finance').setAttribute('data-date', financeData.date);
     document.querySelector('.calendar-science').setAttribute('data-date', scienceData.date);
     document.querySelector('.calendar-international').setAttribute('data-date', internationalData.date);
     document.querySelector('.calendar-dpo').setAttribute('data-date', dpoData.date);
@@ -1139,7 +1197,30 @@ document.addEventListener('DOMContentLoaded', () => {
     insertToPage('students__diagram-info__value_specialists', numDataOutput(staffData.categories.groups.specialists));
     insertToPage('students__diagram-info__value_magisters', numDataOutput(staffData.categories.groups.magisters));
 
+    function setVaccinationtDescription(data) {
+        const description = document.querySelector('.vaccination__diagram-descr span');
 
+        const getlastNum = () => {
+            let array = data.toString().split('');
+
+            return +array[array.length - 1]
+        } 
+
+        const lastNum = getlastNum()
+
+        let str = 'челове';
+
+        if (lastNum === 2 || lastNum === 3 || lastNum === 4) {
+            str += 'ка'
+        } 
+        else {
+            str += 'к'
+        }
+
+        description.textContent = str;
+    }
+
+    setVaccinationtDescription(vaccinationData.total)
 
     // =================== отображение месяца ================================
 
