3ae7755e56e40268b87553b7ffd700deb28af95f
unknown <C:\The Bat!>
2024-05-27 09:36:23 +0300

    upd

--- second-page/structure/src/js/structure.js
+++ second-page/structure/src/js/structure.js
@@ -16,3 +16,29 @@
 //   tab.classList.add('students-enrollment__tab_active')
 //  })
 // })
+
+var modal = document.getElementById("modal");
+var btn1 = document.getElementById("button1");
+var btn2 = document.getElementById("button2");
+var span = document.getElementsByClassName("close")[0];
+var iframe = document.getElementById("modal-iframe");
+
+btn1.onclick = function() {
+  modal.style.display = "block";
+  iframe.src = "https://xn--p1ag3a.xn--p1ai/structure/hs/vyisshaya-injenernaya-shkola-novyie-materialyi-i-tehnologii";
+}
+
+btn2.onclick = function() {
+  modal.style.display = "block";
+  iframe.src = "https://www.example2.com";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
