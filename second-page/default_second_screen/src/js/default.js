c5f46f09e2351f635fbd161b04ee8329a2459b2e
unknown <C:\The Bat!>
2024-05-31 09:20:26 +0300

    u[d

--- second-page/default_second_screen/src/js/default.js
+++ second-page/default_second_screen/src/js/default.js
@@ -11,6 +11,7 @@ const pdfFiles = [
   './assets/defaultSecond/dist/pdf/act10.pdf',
   './assets/defaultSecond/dist/pdf/act11.pdf',
   './assets/defaultSecond/dist/pdf/act12.pdf',
+  './assets/defaultSecond/dist/pdf/act13.pdf',
 ];
 
 pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.6.347/pdf.worker.js';
