51ac2ec81426eceeb371e0b7af5524b66064878d
unknown <C:\The Bat!>
2024-06-07 10:40:34 +0300

    initial

--- second-page/structure/src/js/structure.js
+++ second-page/structure/src/js/structure.js
@@ -1,51 +1,96 @@
-// const tabs = document.querySelectorAll('.students-enrollment__tab')
-// const tabcontent = document.querySelectorAll('.students-enrollment__tabcontent')
-
-// tabs.forEach((tab, index) => {
-//  tab.addEventListener('click', () => {
-//   tabcontent.forEach(item => item.classList.remove('show'))
-//   tabcontent.forEach(item => item.classList.add('hide'))
-//   tabcontent[index].classList.add('show')
-
-//   tabs.forEach(item => {
-//    item.querySelector('svg').querySelector('path').setAttribute('stroke', 'rgba(33, 122, 255, 0.2)')
-//    item.classList.remove('students-enrollment__tab_active')
-//   })
-
-//   tab.querySelector('svg').querySelector('path').setAttribute('stroke', '#217AFF')
-//   tab.classList.add('students-enrollment__tab_active')
-//  })
-// })
-
-
-
-
-
 const modal = document.querySelector("#modal");
-const closeModal = document.querySelector(".close");
+const closeModalButton = document.querySelector(".close");
 const iframe = document.querySelector("#modal-iframe");
-var links = [
-  "https://xn--p1ag3a.xn--p1ai/structure/hs",
-  "https://xn--p1ag3a.xn--p1ai/users",
-  "https://xn--p1ag3a.xn--p1ai/structure",
-];
+const popup = document.querySelector('.popup');
+const popupContent = document.querySelector('.popup__content');
 
-function openModal(index) {
+function openModal(element) {;
   modal.style.display = "block";
-  iframe.src = links[index];
+  iframe.src = element.getAttribute('href');
 }
 
-closeModal.addEventListener('click', () => {
-  modal.style.display = "none";
-  iframe.src = ''
-})
+function closeModal() {
+  modal.style.display = 'none';
+  iframe.src = ''; 
+}
 
-var cards = document.querySelectorAll(".s-card");
+closeModalButton.addEventListener("click", closeModal);
+
+let cards = document.querySelectorAll("a");
 cards.forEach(function(card) {
-  card.addEventListener("click", function() {
-    var index = this.getAttribute("data-link");
-    openModal(index);
+  card.addEventListener("click", function(event) {
+    event.preventDefault();
+    openModal(this);
   });
 });
+
+function openPopup(content) {
+  popupContent.innerHTML = '';
+  popupContent.appendChild(content);
+  popup.classList.add('popup_is_active');
+  popup.addEventListener('click', handleOverlayClosePopup);
+  document.addEventListener('keydown', handleEscClosePopup);
+}
+
+function closePopup(evt) {
+  if (evt.target === evt.currentTarget || evt.target.classList.contains('close')) {
+    popup.classList.remove('popup_is_active');
+    popupContent.innerHTML = '';
+    popup.removeEventListener('click', handleOverlayClosePopup);
+    document.removeEventListener('keydown', handleEscClosePopup);
+  }
+}
+
+function handleOverlayClosePopup(evt) {
+  if (evt.target === evt.currentTarget) {
+    closePopup(evt.currentTarget);
+  }
+}
+
+function handleEscClosePopup(evt) {
+  if (evt.key === 'Escape') {
+    closePopup(document.querySelector('.popup_is_active'));
+  }
+}
+
+function handleEscClosePopup(evt) {
+  if (evt.key === 'Escape') {
+    closePopup(document.querySelector('.popup_is_active'));
+  }
+}
+
+popup.addEventListener('click', closePopup);
+
+function handleClick(elementSelector) {
+  let elements = document.querySelectorAll(elementSelector);
+
+  elements.forEach(function(element) {
+    element.addEventListener('click', function(evt) {
+      evt.preventDefault(); 
+      let content = element.cloneNode(true);
+
+      
+      let brTags = content.querySelectorAll('br');
+      brTags.forEach(function(br) {
+        br.parentNode.removeChild(br);
+      });
+
+      let clonedLinks = content.querySelectorAll('a');
+      clonedLinks.forEach(function(link) {
+        link.addEventListener('click', function(evt) {
+          if(!this.classList.contains('link-unvailable')){
+            evt.preventDefault(); 
+            openModal(link); 
+          }
+   
+        });
+      });
+
+      openPopup(content);
+    });
+  });
+}
+
+handleClick('.scale-on-click');
 
 
