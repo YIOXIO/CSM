90b5295f796a35622bf259138132f485cad02119
unknown <C:\The Bat!>
2024-08-19 12:18:50 +0300

    updated

--- second-page/rating_table/src/js/rating_table.js
+++ second-page/rating_table/src/js/rating_table.js
@@ -1,8 +1,43 @@
 let sortAscending = true;
 
-let rateButton = document.getElementById('rate');
+document.addEventListener('DOMContentLoaded', function() {
+  sortTableByRate();
 
+
+});
+
+const rateButton = document.getElementById('rate');
 rateButton.addEventListener('click', function() {
+  sortTableByRate();
+
+
+});
+
+const numberButton = document.getElementById('number');
+numberButton.addEventListener('click', function() {
+  sortTableByNumber();
+
+});
+
+function sortTableByNumber() {
+  let tableBody = document.querySelector('tbody');
+  let rateElements = Array.from(document.getElementsByClassName('r-table__position'));
+
+  rateElements.sort((a, b) => {
+    let aValue = getNumericValue(a.textContent);
+    let bValue = getNumericValue(b.textContent);
+    return sortAscending ? aValue - bValue : bValue - aValue;
+  });
+
+  for (let i = 0; i < rateElements.length; i++) {
+    let row = rateElements[i].closest('tr');
+    tableBody.appendChild(row);
+  }
+
+  sortAscending = !sortAscending;
+}
+
+function sortTableByRate() {
   let tableBody = document.querySelector('tbody');
   let placeElements = Array.from(document.getElementsByClassName('r-table__place'));
 
@@ -18,36 +53,17 @@ rateButton.addEventListener('click', function() {
   }
 
   sortAscending = !sortAscending;
-});
-
+}
 
 function getNumericValue(value) {
-  if (value.includes('C+')) {
-    return 3;
+  if (value.includes('Срез по бизнес-образованию: место 2')) {
+    return 8;
+  } else if (value.includes('C+')) {
+    return 6;
   } else if (value.includes('-')) {
     let [start, end] = value.split('-').map(Number);
-    return  end; 
+    return end;
   } else {
     return parseInt(value.match(/\d+/)[0]);
   }
 }
-
-let numberButton = document.getElementById('number');
-
-numberButton.addEventListener('click', function() {
-  let tableBody = document.querySelector('tbody');
-  let placeElements = Array.from(document.getElementsByClassName('r-table__position'));
-
-  placeElements.sort((a, b) => {
-    let aValue = getNumericValue(a.textContent);
-    let bValue = getNumericValue(b.textContent);
-    return sortAscending ? aValue - bValue : bValue - aValue;
-  });
-
-  for (let i = 0; i < placeElements.length; i++) {
-    let row = placeElements[i].closest('tr');
-    tableBody.appendChild(row);
-  }
-
-  sortAscending = !sortAscending;
-});
