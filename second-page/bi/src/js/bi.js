c27b5bcbf962a7fe07bbc13b8ecb5e24736e590c
unknown <C:\The Bat!>
2024-08-30 13:29:55 +0300

    updated

--- second-page/bi/src/js/bi.js
+++ second-page/bi/src/js/bi.js
@@ -1,18 +1,63 @@
-// const tabs = document.querySelectorAll('.students-enrollment__tab')
-// const tabcontent = document.querySelectorAll('.students-enrollment__tabcontent')
+const moscowBtn = document.getElementById('moscow-btn');
+const branchBtn = document.getElementById('branch-btn')
+const popup = document.querySelector('.popup')
+const buttonClosePopup = document.querySelector('.popup__close')
+const tabsButtons = document.querySelectorAll('.bi-tabs-button');
+const tabs = document.querySelectorAll('.bi-tab');
 
-// tabs.forEach((tab, index) => {
-//  tab.addEventListener('click', () => {
-//   tabcontent.forEach(item => item.classList.remove('show'))
-//   tabcontent.forEach(item => item.classList.add('hide'))
-//   tabcontent[index].classList.add('show')
 
-//   tabs.forEach(item => {
-//    item.querySelector('svg').querySelector('path').setAttribute('stroke', 'rgba(33, 122, 255, 0.2)')
-//    item.classList.remove('students-enrollment__tab_active')
-//   })
+function openPopup(popupElement) {
+    popupElement.classList.add('popup_is_active');
+    popupElement.addEventListener('click', handleOverlayClosePopup);
+    document.addEventListener('keydown', handleEscClosePopup);
+  }
+  
+function closePopup(popupElement) {
+popupElement.classList.remove('popup_is_active');
+popupElement.removeEventListener('click', handleOverlayClosePopup);
+document.removeEventListener('keydown', handleEscClosePopup);
+}
 
-//   tab.querySelector('svg').querySelector('path').setAttribute('stroke', '#217AFF')
-//   tab.classList.add('students-enrollment__tab_active')
-//  })
-// })
+function handleOverlayClosePopup(evt) {
+if (evt.target === evt.currentTarget) {
+    closePopup(evt.currentTarget);
+}
+}
+
+function handleEscClosePopup(evt) {
+if (evt.key === 'Escape') {
+    closePopup(document.querySelector('.popup_is_active'));
+}
+}
+function switchTab(index) {
+    tabsButtons.forEach(btn => btn.classList.remove('bi-tabs-button_active'));
+    tabs.forEach(tab => tab.classList.remove('bi-tab_active'));
+
+    tabsButtons[index].classList.add('bi-tabs-button_active');
+    tabs[index].classList.add('bi-tab_active');
+}
+
+
+if (moscowBtn) {
+    moscowBtn.addEventListener('click', () => {
+        openPopup(popup);
+        switchTab(0); 
+    });
+}
+
+if (branchBtn) {
+    branchBtn.addEventListener('click', () => {
+        openPopup(popup);
+        switchTab(1); 
+    });
+}
+
+tabsButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        switchTab(index);
+    });
+});
+
+
+moscowBtn.style.cursor="pointer"
+branchBtn.style.cursor="pointer"
